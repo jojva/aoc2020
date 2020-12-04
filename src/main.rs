f338ea8a15917fae4f8d096951bc08ff5904078c
Joris Valette <joris.valette@algolia.com>
2020-12-04 16:20:50 +0100

    improvements

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-const INPUT: &str = include_str!("input");
-const SLOPES: [(usize, usize); 5] = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-const RESULT: u32 = SLOPES.iter().map(|slope| slide(INPUT, slope)).product();
-
 fn main() {
-    println!("{:?}", RESULT);
+    let input = include_str!("input");
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let result: u32 = slopes.iter().map(|slope| slide(input, slope)).product();
+    println!("{:?}", result);
 }
 
-const fn slide(my_str: &str, slope: &(usize, usize)) -> u32 {
+fn slide(input: &str, slope: &(usize, usize)) -> u32 {
     let mut col = 0;
     let mut trees = 0;
-    for line in my_str.lines().step_by(slope.1) {
+    for line in input.lines().step_by(slope.1) {
         trees += has_tree(line, col);
         col += slope.0;
     }
     trees
 }
 
-const fn has_tree(line: &str, col: usize) -> u32 {
+fn has_tree(line: &str, col: usize) -> u32 {
     (line.chars().nth(col % line.len()).unwrap() == '#') as u32
 }
